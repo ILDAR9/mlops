886e554c037d310a7b51ce846c0797d9f3e16e0a
ILDAR9 <1594058+ILDAR9@users.noreply.github.com>
2022-10-08 16:17:39 +0300

    Update mlflow_demo_client.py

--- services/monitoring/src/mlflow_demo_client.py
+++ services/monitoring/src/mlflow_demo_client.py
@@ -2,8 +2,7 @@ import mlflow
 from decouple import config
 import os
 
-# mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_tracking_uri("http://ec2-3-110-179-129.ap-south-1.compute.amazonaws.com:5000")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 mlflow.set_experiment("test-exp")
 
